d95f2d86a5a30cdc021b61fdf10fb8cce581c50c
gordongpan <gordongpan@tencent.com>
2023-06-30 15:49:49 +0800

    feat(core): passthough

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,127 @@
-import { createServer } from 'http'
+import Koa from "koa";
+import koaBodyParser from "koa-bodyparser";
+import Router from "koa-router";
+import KoaStatic from "koa-static";
+import path from "path";
+import { chatConfig, currentModel } from "./chatgpt";
+import { isNotEmptyString } from "./utils/is";
 import { Configuration, OpenAIApi } from 'openai-edge'
 import { OpenAIStream, streamToResponse } from 'ai'
-export const runtime = 'edge'
+
+const app = new Koa();
+const staticPath = "../static";
+// Set the runtime to edge for best performance
+export const runtime = "edge";
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
-
-const server = createServer(async (req, res) => {
+  apiKey: process.env.OPENAI_API_KEY,
+  basePath: 'https://api.openai.com',
+});
+const openai = new OpenAIApi(config);
+app.use(KoaStatic(path.join(__dirname, staticPath)));
+const router = new Router();
+router.get("/", async (ctx) => {
+  ctx.body = {
+    data: "1234",
+  };
+});
+// SSE 请求，不返回标准 JSON，而是 UTF-8 文本
+router.post("/chat-process", async (ctx, next) => {
   const aiResponse = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo-16k-0613',
+    model: 'gpt3',
     stream: true,
-    messages: [{ role: 'user', content: 'What is love?' }]
+    messages: [{ role: 'user', content: 'What is love?' }],
   })
-
   // Transform the response into a readable stream
   const stream = OpenAIStream(aiResponse)
 
   // Pipe the stream to the response
-  streamToResponse(stream, res)
-})
+  streamToResponse(stream, ctx.res)
+});
+// //
+router.post("/config", async (ctx) => {
+  try {
+    const response = await chatConfig();
+    // res.send(response)
+    ctx.body = response;
+  } catch (error) {
+    // res.send(error)
+    ctx.body = error;
+  }
+});
 
-server.listen(3000)
+router.post("/session", async (ctx) => {
+  try {
+    const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY;
+    const hasAuth = isNotEmptyString(AUTH_SECRET_KEY);
+    // res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
+    ctx.body = {
+      status: "Success",
+      message: "",
+      data: { auth: hasAuth, model: currentModel() },
+    };
+  } catch (error) {
+    // res.send({ status: 'Fail', message: error.message, data: null })
+    ctx.body = { status: "Fail", message: error.message, data: null };
+  }
+});
+
+router.post("/verify", async (ctx, next) => {
+  try {
+    const { token } = ctx.request.body as { token: string };
+    if (!token) throw new Error("Secret key is empty");
+
+    if (process.env.AUTH_SECRET_KEY !== token)
+      throw new Error("密钥无效 | Secret key is invalid");
+
+    // res.send({ status: 'Success', message: 'Verify successfully', data: null })
+    ctx.body = {
+      status: "Success",
+      message: "Verify successfully",
+      data: null,
+    };
+  } catch (error) {
+    // res.send({ status: 'Fail', message: error.message, data: null })
+    ctx.body = { status: "Fail", message: error.message, data: null };
+  }
+});
+const home = new Router();
+home.get("/test", async (ctx) => {
+  const res = ctx.res;
+  ctx.status = 200;
+  ctx.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    "Transfer-Encoding": "chunked",
+  });
+  res.write(`start<br>`);
+  return new Promise<void>((resolve) => {
+    let i = 0,
+      total = 5;
+    while (i <= total) {
+      (function (i) {
+        setTimeout(() => {
+          if (i === total) {
+            resolve();
+            res.end();
+          } else {
+            res.write(`${i}<br>`);
+          }
+        }, i * 1000);
+      })(i);
+      i++;
+    }
+  });
+});
+const rootRouter = new Router();
+// // 装载所有子路由
+// let router = new Router()
+rootRouter.use("/home", home.routes(), home.allowedMethods());
+rootRouter.use("/api", router.routes(), router.allowedMethods());
+app.use(koaBodyParser());
+// router.use('/api', router.routes(), router.allowedMethods())
+// 加载路由中间件
+app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
+app.listen(process.env.PORT || 9020, () => {
+  console.log("启动了");
+});
