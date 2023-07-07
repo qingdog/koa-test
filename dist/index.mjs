e6c30caf3430195459ab2551dd55346ee64077fe
gordongpan <gordongpan@tencent.com>
2023-07-07 16:10:36 +0800

    feat(core): passthough

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,13 +1,21 @@
 // src/index.ts
-import { createServer } from "http";
+import express from "express";
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, streamToResponse } from "ai";
+var app = express();
+var router = express.Router();
 var runtime = "edge";
 var config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
+app.use(express.static("public"));
+app.use(express.json());
+app.all("*", (_, res, next) => {
+  next();
+});
 var openai = new OpenAIApi(config);
-var server = createServer(async (req, res) => {
+router.post("/chat-process", [], async (req, res) => {
+  res.setHeader("Content-type", "application/octet-stream");
   const aiResponse = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-16k-0613",
     stream: true,
@@ -16,7 +24,6 @@ var server = createServer(async (req, res) => {
   const stream = OpenAIStream(aiResponse);
   streamToResponse(stream, res);
 });
-server.listen(3e3);
 export {
   runtime
 };
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
-import { createServer } from 'http'
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, streamToResponse } from 'ai'
-export const runtime = 'edge'
+import express from "express";
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, streamToResponse } from "ai";
+const app = express();
+const router = express.Router();
+export const runtime = "edge";
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
+  apiKey: process.env.OPENAI_API_KEY,
+});
+app.use(express.static("public"));
+app.use(express.json());
 
-const server = createServer(async (req, res) => {
+app.all("*", (_, res, next) => {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "authorization, Content-Type");
+  // res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+const openai = new OpenAIApi(config);
+router.post("/chat-process", [], async (req, res) => {
+  res.setHeader("Content-type", "application/octet-stream");
+
   const aiResponse = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo-16k-0613',
+    model: "gpt-3.5-turbo-16k-0613",
     stream: true,
-    messages: [{ role: 'user', content: 'What is love?' }]
-  })
+    messages: [{ role: "user", content: "What is love?" }],
+  });
 
   // Transform the response into a readable stream
-  const stream = OpenAIStream(aiResponse)
+  const stream = OpenAIStream(aiResponse);
 
   // Pipe the stream to the response
-  streamToResponse(stream, res)
-})
-
-server.listen(3000)
+  streamToResponse(stream, res);
+});
+// const server = createServer(async (req, res) => {
+//   const aiResponse = await openai.createChatCompletion({
+//     model: 'gpt-3.5-turbo-16k-0613',
+//     stream: true,
+//     messages: [{ role: 'user', content: 'What is love?' }]
+//   })
+//
+//   // Transform the response into a readable stream
+//   const stream = OpenAIStream(aiResponse)
+//
+//   // Pipe the stream to the response
+//   streamToResponse(stream, res)
+// })
+//
+// server.listen(3000)
