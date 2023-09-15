fb6b1a16d70d3be3afd0b6a001156bb880f66de9
qingdog <1759765836@qq.com>
2023-09-15 14:34:03 +0800

    add-OPENAI_BASE_PATH

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,42 @@
-import { createServer } from 'http'
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, streamToResponse } from 'ai'
+import {createServer} from 'http'
+import {Configuration, OpenAIApi} from 'openai-edge'
+import {OpenAIStream, streamToResponse} from 'ai'
+import * as fs from "fs";
+import * as path from "path";
 
+const dotenv = require('dotenv');
+dotenv.config();
 const OpenAIConfig = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+    basePath: process.env.OPENAI_BASE_PATH || 'https://api.openai.com/v1',
+    apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(OpenAIConfig)
 const runtime = "edge";
 const config = {
-  supportsResponseStreaming: true
+    supportsResponseStreaming: true
 };
+
 const server = createServer(async (req, res) => {
-  const aiResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-16k-0613",
-    stream: true,
-    messages: [{ role: 'user', content: 'What is love?' }]
-  })
+    if (req.url === '/v1/chat/completions') {
+        const aiResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            stream: true,
+            messages: [{ role: 'user', content: 'What is love?' }]
+        })
 
-  // Transform the response into a readable stream
-  const stream = OpenAIStream(aiResponse)
+        // Transform the response into a readable stream
+        const stream = OpenAIStream(aiResponse)
 
-  // Pipe the stream to the response
-  streamToResponse(stream, res)
+        // Pipe the stream to the response
+        streamToResponse(stream, res)
+    } else {
+        const uri = req.url === '/' ? '/index.html': req.url;
+        fs.readFile(path.join(__dirname, '../public' + uri), (err, data) => {
+            res.setHeader('Content-Type', 'text/html;charset=utf-8')
+            res.end(data?.toString())
+        })
+
+    }
 })
 
 server.listen(3000)
--- public/script.js
+++ public/script.js
@@ -1,12 +1,22 @@
 const fetchData = async () => {
-  const apiUrl = localStorage.getItem("apiUrl");
-  if (apiUrl) {
-    const respone = await fetch(`${apiUrl}/users`);
-    const data = await respone.json();
-    console.log(data);
-  } else {
-    window.location.href = "/api";
-  }
+    const apiUrl = localStorage.getItem("apiUrl");
+    console.log(apiUrl)
+    if (apiUrl) {
+        const respone = await fetch(`${apiUrl}/users`);
+        const data = await respone.json();
+        console.log(data);
+    } else {
+        // window.location.href = "/api";
+        const response = await fetch(`v1/chat/completions`);
+        const readableStreamDefaultReader = response.body.getReader();
+
+        const decoder = new TextDecoder();
+        let value;
+        while (!({value} = await readableStreamDefaultReader.read()).done) {
+            console.info(decoder.decode(value))
+        }
+
+    }
 };
 
 fetchData();
